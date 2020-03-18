1ae8e18bfcfd2ad62e4e70799e05719a8aa25a67
Andrew Elagin <andrewelagin@Andrews-Mac-mini.local>
2020-03-18 10:56:36 +0200

    made DnD in one column

--- src/index.js
+++ src/index.js
@@ -1,12 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { columnsFromBackend } from './mockedData';
+
+const onDragEnd = (result, columns, setColumns) => {
+  if(!result.destination) return;
+  const { source, destination } = result;
+  const column = columns[source.droppableId];
+
+  const copiedItems = [...column.items];
+  const [removed] = copiedItems.splice(source.index, 1);
+  copiedItems.splice(destination.index, 0, removed);
+
+  setColumns({
+    ...columns,
+    [source.droppableId]: {
+      ...column,
+      items: copiedItems
+    }
+  })
+}
+
+const App = () => {
+
+  const [columns, setColumns] = useState(columnsFromBackend)
+  return (
+    <div className="content">
+      <DragDropContext onDragEnd={result => onDragEnd(result, columns, setColumns)}>
+        {Object.entries(columns).map(([id, column]) => {
+          return (
+            <Droppable key={id} droppableId={id}>
+              {(provided, snapshot) => {
+                return (
+                  <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    style={{
+                      background: snapshot.isDraggingOver ? 'lightblue' : 'lightgray',
+                      padding: '4px',
+                      width: 250,
+                      minHeight: 500
+                    }}
+                  >
+                    {column.items.map((item, index) => {
+                      return (
+                        <Draggable key={item.id} draggableId={item.id} index={index}>
+                          {(provided, snapshot) => {
+                            return (
+                              <div
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                                ref={provided.innerRef}
+                                style={{
+                                  userSelect: 'none',
+                                  padding: 16,
+                                  margin: '0 0 8px 0',
+                                  minHeight: 50,
+                                  backgroundColor: snapshot.isDragging ? '#263B4A' : '#456C86',
+                                  color: 'white',
+                                  ...provided.draggableProps.style
+                                }}
+                              >
+                                {item.content}
+                              </div>
+                            )
+                          }}
+                        </Draggable>
+                      )
+                    })}
+                    {provided.placeholder}
+                  </div>
+                )
+              }}
+            </Droppable>
+          )
+        })}
+      </DragDropContext>
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ src/mockedData.js
@@ -0,0 +1,16 @@
+import { v4 as uuid } from 'uuid';
+
+export const itemsFromBackend = [
+  { id: uuid(), content: "First task" },
+  { id: uuid(), content: "Second task" },
+  { id: uuid(), content: "Third task" },
+  { id: uuid(), content: "Fourth task" },
+  { id: uuid(), content: "Fifth task" }
+];
+
+export const columnsFromBackend = {
+  [uuid()]: {
+    name: "Requested",
+    items: itemsFromBackend
+  },
+};
